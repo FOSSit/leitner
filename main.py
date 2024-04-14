f94fff4e9596137c388f54b88a959eaf45777775
koshambiB <bardhankoshambi2005@gmail.com>
2024-04-14 17:49:26 +0530

    beautifying the code

--- main.py
+++ main.py
@@ -3,39 +3,39 @@ from random import randint
 
 def main():
     slots = (list(cards.items()), [], [])
-    box_chance_mul = [4, 2, 1]
+    box_chance_multiplier = [4, 2, 1]
     while True:
-        wts = [len(i) * box_chance_mul[idx] for idx, i in enumerate(slots)]
-        f = randint(1, sum(wts)) - 1
-        box_idx = 0
-        a = 0
-        n = 0
-        for idx, i in enumerate(wts):
-            a += i
-            if f < a:
-                box_idx = idx
-                n = f - a + i
+        weights = [len(i) * box_chance_multiplier[index] for index, i in enumerate(slots)]
+        f = randint(1, sum(weights)) - 1
+        box_index = 0
+        answer = 0
+        n = 0 # calculates the position of the card within that slot
+        for index, i in enumerate(weights):
+            answer += i
+            if f < answer:
+                box_index = index
+                n = f - answer + i
                 break
-        box = slots[box_idx]
-        q, a = box.pop(n // box_chance_mul[box_idx])
-        print(chr(27) + "2J")
-        # print(box_idx, f, n)
-        print(q)
+        box = slots[box_index]
+        question, answer = box.pop(n // box_chance_multiplier[box_index])
+        print(chr(27) + "[2J")
+        # print(box_index, f, n)
+        print(question)
         print("~" * 10)
         input("The answer is : ")
-        o = input(f" The answer was : {a}\n Was the answer correct? (Y/N) : ")
+        option = input(f" The answer was : {answer}\n Was the answer correct? (Y/N) : ")
         print("~" * 10)
-        if not o or o[0].lower() == "Y":
-            box_idx = min(box_idx + 1, len(slots) - 1)
-        elif o[0].lower() == "N":
-            box_idx = max(box_idx - 1, 0)
+        if not option or option[0].lower() == "Y":
+            box_index = min(box_index + 1, len(slots) - 1)
+        elif option[0].lower() == "N":
+            box_index = max(box_index - 1, 0)
         else:
             break
-        slots[box_idx].append((q, a))
+        slots[box_index].append((question, answer))
         if len(cards) == len(slots[-1]):
             print(f" You have memorised all {len(cards)} cards")
             k = input("Do you want to exit? (Y/N) : ")
-            if o and o[0].lower() == "Y":
+            if option and option[0].lower() == "Y":
                 break
 
 if __name__ == "__main__":
